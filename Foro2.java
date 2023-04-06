252ebd8af1d65a9fab7fb1afa794de76c4226e13
ItsJevoo <130007538+ItsJevoo@users.noreply.github.com>
2023-04-05 19:04:32 -0500

    Add files via upload

---
+++ Foro2.java
@@ -0,0 +1,23 @@
+public class Foro2 {
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+        selectionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
